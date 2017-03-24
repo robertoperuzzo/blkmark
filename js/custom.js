315071285aba72fc6cf0478d7717a569a45e4588
Roberto Peruzzo <roberto.peruzzo@gmail.com>
2017-03-24 13:58:01 +0100

    Added background photos and js animation effects.

---
+++ js/custom.js
@@ -0,0 +1,29 @@
+$(function(){
+
+  /*
+  Mouse over Magazine Logo.
+   */
+  $('#tsk-logo').hover(function() {
+    $('#no-bw').hide();
+    $('#no-bw-crossed').removeClass('hidden');
+    $('#bottom-black-photo').removeClass('hidden');
+  }, function() {
+    $('#no-bw').show();
+    $('#no-bw-crossed').addClass('hidden');
+    $('#bottom-black-photo').addClass('hidden');
+  });
+
+  /*
+  Mouse over BlkMark Logo.
+   */
+  $('#blk-logo').hover(function() {
+    $('#no-color').hide();
+    $('#no-color-crossed').removeClass('hidden');
+    $('#top-white-photo').removeClass('hidden');
+  }, function() {
+    $('#no-color').show();
+    $('#no-color-crossed').addClass('hidden');
+    $('#top-white-photo').addClass('hidden');
+  });
+
+});
